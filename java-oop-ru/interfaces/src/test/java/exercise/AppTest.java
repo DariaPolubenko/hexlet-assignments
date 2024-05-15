c2285a96cb28446f33b10c7b7f92f3bad85016e9
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-16 02:36:25 +0700

    update AppTest.java

--- java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
+++ java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
@@ -57,6 +57,12 @@ class AppTest {
     }
 
     // BEGIN
-    
+    @Test
+    void testReversedSequence() {
+        CharSequence text = new ReversedSequence("abcdef");
+        assertThat(text.charAt(1)).isEqualTo('e');
+        assertThat(text.length()).isEqualTo(6);
+        assertThat(text.subSequence(1, 4).toString()).isEqualTo("edc");
+    }
     // END
 }
