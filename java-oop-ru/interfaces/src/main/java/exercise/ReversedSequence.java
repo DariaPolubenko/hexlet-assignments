437538a587e6eebd6dff8090b4ceddbe8aeddacc
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-16 02:27:22 +0700

    update ReversedSequence.java

--- java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
+++ java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
@@ -1,6 +1,8 @@
+/*
 package exercise;
 
 import org.apache.commons.lang3.StringUtils;
+import java.lang.CharSequence;
 
 
 // BEGIN
@@ -10,5 +12,17 @@ public class ReversedSequence implements CharSequence {
     public ReversedSequence(String line) {
         this.line = StringUtils.reverse(line);
     }
+
+    @Override
+    public String subSequence(int start, int end) {
+       return line.substring(start, end);
+    }
+
+    @Override
+    public static charAt(int) {
+
+    }
 }
 // END
+
+ */
