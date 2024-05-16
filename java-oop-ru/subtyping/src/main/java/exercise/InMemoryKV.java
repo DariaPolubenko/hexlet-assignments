da3d5e796d6f9295959be6930825a9ef54f9bc95
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-16 22:55:20 +0700

    update InMemoryKV.java

--- java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -4,5 +4,27 @@ import java.util.Map;
 import java.util.HashMap;
 
 // BEGIN
+public class InMemoryKV KeyValueStorage implements KeyValueStorage {
+    private Map<String, String> storage;
 
+    public KeyValueStorage(Map<String, String> initialValue) {
+        this.storage = initialValue;
+    }
+
+    public void set(String key, String value) {
+        storage.put(key, value);
+    }
+
+    public void unset(String key) {
+        storage.remove(key);
+    }
+
+    public String get(String key, String defaultValue) {
+            return storage.getOrDefault(key, defaultValue);
+    }
+
+    public Map<String, String> toMap() {
+        return storage;
+    }
+}
 // END
