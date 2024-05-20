755dd7d3ce4271311edfe15cf12d741dc0dfeb30
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-21 06:13:09 +0700

    update Tag.java

--- java-oop-ru/inheritance/src/main/java/exercise/Tag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/Tag.java
@@ -6,33 +6,24 @@ import java.util.Map;
 // BEGIN
 public abstract class Tag {
     private String name;
-    private Map<String, String> tag;
+    private Map<String, String> attributes;
 
-    public Tag(String name, Map<String, String> tag) {
+    public Tag(String name, Map<String, String> attributes) {
         this.name = name;
-        this.tag = tag;
+        this.attributes = attributes;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getStringTag() {
-        if (!tag.isEmpty()) {
-            var result = new StringBuilder(" ");
-            for (var entry : tag.entrySet()) {
-                result.append(entry.getKey() + "=\"");
-                result.append(entry.getValue() + "\" ");
-            }
-            result.toString();
-            var result1 = result.substring(0, result.length() - 1);
-            return result1;
-        }
-        return "";
-    }
-
-    public Map<String, String> getTag() {
-        return tag;
+    public String stringifyAttributes() {
+        return attributes.keySet().stream()
+                .map(key -> {
+                    var value = attributes.get(key);
+                    return String.format(" %s=\"%s\"", key, value);
+                })
+                .collect(Collectors.joining(""));
     }
 
     public abstract String toString();
