da1fe80f8d0bf2a7a0c55c4e998ae9cfaef2fbab
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-20 21:14:50 +0700

    create LabelTag.java

--- java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -1,5 +1,17 @@
 package exercise;
 
 // BEGIN
+public class LabelTag implements TagInterface {
+    private String tag;
+    private TagInterface label;
 
+    public LabelTag(String tag, TagInterface label) {
+        this.tag = tag;
+        this.label = label;
+    }
+
+    public String render() {
+        return "<label>" + this.tag + this.label.render() + "</label>";
+    }
+}
 // END
