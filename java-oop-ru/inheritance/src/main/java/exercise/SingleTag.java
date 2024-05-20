3acfb082639610d16d9b006e88535bc826c2a304
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-21 05:52:01 +0700

    update SingleTag.java

--- java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/SingleTag.java
@@ -1,7 +1,29 @@
 package exercise;
 
+import java.util.List;
 import java.util.Map;
 
 // BEGIN
+public class SingleTag extends Tag {
 
+    public SingleTag(String name, Map<String, String> tag) {
+        super(name, tag);
+    }
+
+    @Override
+    public String toString() {
+        if (getStringTag().isEmpty()) {
+            return "<" + this.getName() + ">";
+        }
+        return "<" + this.getName() + getStringTag() + ">";
+    }
+
+    public String toStringChild() {
+        if (getStringTag().isEmpty()) {
+            return "";
+        }
+        return "<" + getName() + getStringTag() + ">";
+    }
+
+}
 // END
