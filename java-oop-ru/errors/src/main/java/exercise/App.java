2eb421dc74d573d4780dc132e29b36ab1ed6bf86
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-22 01:47:09 +0700

    update App.java

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -1,5 +1,26 @@
 package exercise;
 
 // BEGIN
+public class App {
+    public static void printSquare(Circle circle) {
 
+        var square = 0;
+        try {
+            square = (int) Math.ceil(circle.getSquare());
+            System.out.println(square);
+        } catch (Exception e) {
+            System.out.println("Не удалось посчитать площадь");
+        } finally {
+            System.out.println("Вычисление окончено");
+        }
+    }
+
+    public static void main(String[] args) throws NegativeRadiusException {
+        var point = new Point(1, 2);
+        var circle = new Circle(point, 2);
+
+        System.out.println(circle.getSquare());
+        System.out.println((int) circle.getSquare());
+    }
+}
 // END
