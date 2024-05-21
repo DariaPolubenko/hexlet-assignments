c693d177a37c04ffb78e71786a00eee46ec93f13
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-22 04:44:43 +0700

    update Validator.java

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -1,6 +1,32 @@
 package exercise;
 
 import java.lang.reflect.Field;
-// BEGIN
+import java.util.ArrayList;
+import java.util.List;
 
+// BEGIN
+public class Validator {
+    public static List<String> validate(Address address) throws RuntimeException {
+        var notValidFields = new ArrayList<String>();
+        Field[] fields = address.getClass().getDeclaredFields();
+
+        try {
+            for (Field field : fields) {
+                var notNull = field.getAnnotation(NotNull.class);
+                field.setAccessible(true);
+                Object value = field.get(address);
+
+                if (value == null & notNull != null) {
+                    String result = field.getName();
+                    notValidFields.add(result);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+
+        return notValidFields;
+    }
+
+}
 // END
