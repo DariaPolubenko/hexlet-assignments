934b24a1b6cb68f602769976c4dc9179c77ff15d
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-22 01:50:29 +0700

    update App.java

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -14,13 +14,5 @@ public class App {
             System.out.println("Вычисление окончено");
         }
     }
-
-    public static void main(String[] args) throws NegativeRadiusException {
-        var point = new Point(1, 2);
-        var circle = new Circle(point, 2);
-
-        System.out.println(circle.getSquare());
-        System.out.println((int) circle.getSquare());
-    }
 }
 // END
