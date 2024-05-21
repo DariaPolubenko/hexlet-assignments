bfa346eb45c9cb2a6073487fe933841a9837eb2e
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-22 01:47:20 +0700

    update Circle.java

--- java-oop-ru/errors/src/main/java/exercise/Circle.java
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -1,5 +1,26 @@
 package exercise;
 
 // BEGIN
+public class Circle {
+    private Point point;
+    private int radius;
+    private final double PI = 3.14159265359;
 
+    public Circle(Point point, int radius) {
+        this.point = point;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public double getSquare() throws NegativeRadiusException {
+        if (radius < 0) {
+            var error = new NegativeRadiusException("");
+            throw error;
+        }
+        return radius * radius * PI;
+    }
+}
 // END
