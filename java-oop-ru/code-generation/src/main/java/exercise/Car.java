142c50e72f88e1f0803d32301fcd4ea66022e740
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-22 22:41:21 +0700

    update Car.java

--- java-oop-ru/code-generation/src/main/java/exercise/Car.java
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -1,10 +1,14 @@
 package exercise;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.SneakyThrows;
 import lombok.Value;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 // BEGIN
-
+@Getter
+@AllArgsConstructor
 // END
 class Car {
     int id;
@@ -14,6 +18,14 @@ class Car {
     User owner;
 
     // BEGIN
-    
+    @SneakyThrows
+    public String serialize() {
+        return new ObjectMapper().writeValueAsString(this);
+    }
+
+    @SneakyThrows
+    public static Car unserialize(String car) {
+        return new ObjectMapper().readValue(car, Car.class);
+    }
     // END
 }
