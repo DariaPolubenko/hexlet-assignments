345bec84b0108b4296b1e9dccde8d5525b1019b0
Daria Polubenko <daria.polubenko@gmail.com>
2024-05-22 22:41:06 +0700

    update App.java

--- java-oop-ru/code-generation/src/main/java/exercise/App.java
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -1,9 +1,26 @@
 package exercise;
 
+import lombok.SneakyThrows;
+
 import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
-// BEGIN
+import static exercise.Car.unserialize;
+import static java.nio.file.Files.readString;
 
+// BEGIN
+public class App {
+    @SneakyThrows
+    public static void save(Path path, Car car) {
+        var stringJson = car.serialize();
+        Files.writeString(path, stringJson);
+    }
+
+    @SneakyThrows
+    public static Car extract(Path path) {
+        var content = readString(path);
+        return unserialize(content);
+    }
+}
 // END
