f80b92a9138eeda90c35df7de2825cf1fc887702
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-19 02:56:34 +0700

    update build.gradle.kts

--- java-web-ru/javalin-intro/build.gradle.kts
+++ java-web-ru/javalin-intro/build.gradle.kts
@@ -20,23 +20,23 @@ repositories {
 
 dependencies {
     // BEGIN
-    
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
     // END
     implementation("org.slf4j:slf4j-simple:2.0.7")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.konghq:unirest-java:3.13.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
+
 }
 
 tasks.test {
     useJUnitPlatform()
-    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
     testLogging {
         exceptionFormat = TestExceptionFormat.FULL
         events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-        // showStackTraces = true
-        // showCauses = true
         showStandardStreams = true
     }
 }
