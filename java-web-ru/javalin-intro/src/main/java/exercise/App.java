ecac49eac52b77349475d5ce119885c4dfe3821a
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-19 02:56:44 +0700

    update App.java

--- java-web-ru/javalin-intro/src/main/java/exercise/App.java
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -1,15 +1,17 @@
 package exercise;
 
 // BEGIN
-
+import io.javalin.Javalin;
 // END
 
 public final class App {
 
     public static Javalin getApp() {
-
         // BEGIN
-        
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        return app.get("/welcome", ctx -> ctx.result("Welcome to Hexlet!"));
         // END
     }
 
