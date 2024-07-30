42dbd4716d33e6cf32a264e787004639b9dece61
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-31 02:58:35 +0700

    update PostsController.java

--- java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -2,8 +2,8 @@ package exercise.controller;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
-import exercise.dto.posts.PostsPage;
 import exercise.dto.posts.PostPage;
+import exercise.dto.posts.PostsPage;
 import exercise.repository.PostRepository;
 
 import io.javalin.http.Context;
@@ -12,6 +12,18 @@ import io.javalin.http.NotFoundResponse;
 public class PostsController {
 
     // BEGIN
-    
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var post = PostRepository.find(id).orElseThrow(() -> new NotFoundResponse("Page not found"));
+        var page = new PostPage(post);
+        ctx.render("posts/show.jte", model("page", page));
+    }
+
+    public static void index(Context ctx) {
+        var posts = PostRepository.getEntities();
+        var page = new PostsPage(posts);
+        ctx.render("posts/index.jte", model("page", page));
+    }
+
     // END
 }
