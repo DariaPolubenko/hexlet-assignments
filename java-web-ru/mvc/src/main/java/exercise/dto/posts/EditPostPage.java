e5420ddb40d16fa1649f87fc49f00b9fbf44fd49
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-31 23:15:06 +0700

    create EditPostPage.java

--- java-web-ru/mvc/src/main/java/exercise/dto/posts/EditPostPage.java
+++ java-web-ru/mvc/src/main/java/exercise/dto/posts/EditPostPage.java
@@ -2,11 +2,20 @@ package exercise.dto.posts;
 
 import java.util.List;
 import java.util.Map;
+
+import exercise.model.Post;
 import io.javalin.validation.ValidationError;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 // BEGIN
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class EditPostPage {
+    private String name;
+    private String body;
+    private Map<String, List<ValidationError<Object>>> errors;
+}
 // END
