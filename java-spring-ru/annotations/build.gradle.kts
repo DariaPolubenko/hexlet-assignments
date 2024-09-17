1b94caa7146fcddc319de7d70a707acd569a6781
@hexlet/cli <support@hexlet.io>
2024-09-17 20:21:28 +0700

    submit java-spring-ru/annotations

---
+++ java-spring-ru/annotations/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    id("com.github.ben-manes.versions") version "0.48.0"
+}
+
+group = "exercise"
+
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("exercise.Application") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
