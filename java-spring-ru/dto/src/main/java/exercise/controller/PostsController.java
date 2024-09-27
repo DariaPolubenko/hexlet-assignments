9583a422e974170ffb7a99463db4a20106c958af
Daria Polubenko <daria.polubenko@gmail.com>
2024-09-28 00:02:18 +0300

    update PostsController.java

--- java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -3,10 +3,9 @@ package exercise.controller;
 import exercise.model.Comment;
 import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 import exercise.model.Post;
@@ -32,6 +31,7 @@ public class PostsController {
     }
 
     @GetMapping("/posts/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public PostDTO show(@PathVariable Long id) {
         var post = postRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
